b5cd41058809972d89b6785f0cb5b65036f94ac7
Jorge Obiols <jorge.obiols@gmail.com>
2023-09-08 09:59:17 -0300

    [FIX] repos

--- make_readme.sh
+++ make_readme.sh
@@ -1,15 +1,10 @@
 #!/usr/bin/env bash
-##############################################################################
-# Genera la documentacion de los modulos, requiere la instalacion de oca
-# maintainers tools en tu maquina.
-# bajalo de aca --> https://github.com/OCA/maintainer-tools y lo instalas asi:
+#####################################################################################
+# Genera la documentacion de los modulos, requiere la instalacion de gen-odoo-readme
+# Lo podes instalar desde el pip asi:
 #
-# $ git clone git@github.com:OCA/maintainer-tools.git
-# $ cd maintainer-tools
-# $ virtualenv env
-# $ . env/bin/activate
-# $ python setup.py install
-#
+# $ pip install gen-odoo-readme
+
 gen-readme \
 	--org-name quilsoft-org \
 	--repo-name utest \
