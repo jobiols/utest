979fb7a47b018c0600dd5ca0bf70c4a1b0ee081e
Jorge Obiols <jorge.obiols@gmail.com>
2024-05-12 00:48:46 -0300

    [WIP]

--- module_2/tests/test_resta.py
+++ module_2/tests/test_resta.py
@@ -5,4 +5,4 @@ class DocumentTestRestaCase(TransactionCase):
     def test_01_resta(self):
 
         partner_obj = self.env["res.partner"]
-        self.assertEqual(partner_obj._resta(4, 2), 4 + 2)
+        self.assertEqual(partner_obj._resta(4, 2), 4 - 2)
