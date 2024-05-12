3202ae6e529de06be54a2fccd01b6bd24cb9112f
Jorge Obiols <jorge.obiols@gmail.com>
2024-05-12 01:19:05 -0300

    [WIP]

--- module_1/models/res_partner.py
+++ module_1/models/res_partner.py
@@ -8,3 +8,12 @@ class ResPartner(models.Model):
 
     def _suma(self, a, b):
         return a + b
+
+    def total_partners(self):
+        """ calcula el total de partners """
+
+        total = self.search([])
+        i = 0
+        for a in total:
+            i = i + 1
+        print(i)
